4ae0bb3eea5357681a236ff7d68131510a1fe82e
Andreas Nüßing <andreas.nuessing@uni-muenster.de>
2015-06-15 14:49:21 +0200

    [Test3] add time comparison on different stages
    we now also output the additional time including iteration and integration
    to get a better feeling in real world situations.

--- src/tpmc_test_3_performance.cc
+++ src/tpmc_test_3_performance.cc
@@ -5,6 +5,7 @@
 #include <tpmc/marchingcubes.hh>
 #include "grid.hh"
 #include "timer.hh"
+#include "geometry.hh"
 
 namespace tpmc
 {
@@ -14,6 +15,40 @@ namespace tpmc
     typedef T field_type;
   };
 }
+
+struct TimeResult
+{
+  // the time for computing the key for each grid element
+  double key;
+  // the time for computing the surface area of the interface
+  double iteration;
+  // total time, including grid iteration
+  double total;
+  // area of the interface
+  double area;
+
+  double elements;
+
+  TimeResult& operator+=(const TimeResult& other)
+  {
+    key += other.key;
+    iteration += other.iteration;
+    total += other.total;
+    area += other.area;
+    elements += other.elements;
+    return *this;
+  }
+
+  TimeResult& operator/=(double v)
+  {
+    key /= v;
+    iteration /= v;
+    total /= v;
+    area /= v;
+    elements /= v;
+    return *this;
+  }
+};
 
 // simple functor which computes the time needed for calculating the keys for all elements
 template <int dim, class domain_type>
@@ -28,9 +63,11 @@ public:
   {
   }
 
-  double operator()(const std::vector<field_type>& data) const
+  TimeResult operator()(const std::vector<field_type>& data) const
   {
-    double time = 0;
+    TimeResult result{ 0.0, 0.0, 0.0, 0.0, 0 };
+    tpmc_test::Timer timer;
+    tpmc_test::Timer iterationTimer;
     // loop through all elements
     for (const auto& element : grid_.elements()) {
       // extract corner values of current elements
@@ -38,22 +75,65 @@ public:
       for (unsigned int i = 0; i < element.cornerCount(); ++i) {
         cornerValues.push_back(data[grid_.cornerIndex(element, i)]);
       }
+      tpmc_test::Timer keyTimer;
       // calculate key
-      tpmc_test::Timer timer;
       std::size_t key = mc33_.getKey(cornerValues.begin(), cornerValues.end());
-      // add current time to total count
-      time += timer.total().count();
+      result.key += keyTimer.total().count();
+
+      result.iteration += iterationTimer.lap().count();
+
+      // compute surface area
+      std::vector<domain_type> vertices;
+      mc33_.getVertices(std::begin(cornerValues), std::end(cornerValues), key,
+                        std::back_inserter(vertices));
+      std::vector<std::vector<int> > faces;
+      mc33_.getElements(tpmc::makeGeometryType(dim, element.cornerCount()), key,
+                        tpmc::ReconstructionType::Interface, std::back_inserter(faces));
+      result.elements += faces.size();
+      auto toGlobal =
+          [&](int c) { return c < 0 ? element.corner(-c - 1) : element.global(vertices[c]); };
+      for (const auto& face : faces) {
+        // transform face to global coordinates
+        std::vector<domain_type> global;
+        std::transform(face.begin(), face.end(), std::back_inserter(global), toGlobal);
+        // add the area of global
+        result.area += tpmc_test::area<field_type>(global.begin(), global.end());
+      }
+
+      iterationTimer.lap();
     }
-    return time;
+    result.total = timer.total().count();
+    return result;
   }
 
-  const tpmc::MarchingCubes<field_type, dim, domain_type>& getMC() const {
-    return mc33_;
-  }
+  const tpmc::MarchingCubes<field_type, dim, domain_type>& getMC() const { return mc33_; }
+
 private:
   tpmc_test::Grid<dim> grid_;
   tpmc::MarchingCubes<field_type, dim, domain_type> mc33_;
 };
+
+template <class Map>
+void printStatistics(std::ostream& output, const Map& m)
+{
+  typedef typename decltype(m.begin()->second)::value_type field_type;
+  output << "N min max mean std\n";
+  for (auto x : m) {
+    field_type min
+        = std::accumulate(x.second.begin(), x.second.end(), std::numeric_limits<field_type>::max(),
+                          [](field_type a, field_type b) { return std::min(a, b); });
+    field_type max
+        = std::accumulate(x.second.begin(), x.second.end(), std::numeric_limits<field_type>::min(),
+                          [](field_type a, field_type b) { return std::max(a, b); });
+    field_type mean = std::accumulate(x.second.begin(), x.second.end(), 0.0) / x.second.size();
+    field_type st = 0.0;
+    for (auto v : x.second) {
+      st += (v - mean) * (v - mean);
+    }
+    st = std::sqrt(st / (x.second.size() - 1));
+    output << x.first << " " << min << " " << max << " " << mean << " " << st << "\n";
+  }
+}
 
 int main(int argc, char** argv)
 {
@@ -64,7 +144,8 @@ int main(int argc, char** argv)
   const std::string outputfilename = argv[1];
   // define general grid properties
   const int dim = 3;
-  const std::vector<unsigned int> numbersOfElements{ { 16, 32, 64, 128, 256 } };
+  // const std::vector<unsigned int> numbersOfElements{ { 16, 32, 64, 128, 256 } };
+  const std::vector<unsigned int> numbersOfElements{ { 16, 32 } };
   const unsigned int numberOfRandomRuns = 30;
   const unsigned int numberOfRunsPerDataset = 30;
   typedef tpmc_test::Grid<dim>::domain_type domain_type;
@@ -76,10 +157,13 @@ int main(int argc, char** argv)
   std::default_random_engine generator(seed);
   std::uniform_real_distribution<field_type> distribution(-1.0, 1.0);
 
-  std::map<unsigned int, std::vector<field_type> > timeRatios;
-  std::map<unsigned int, unsigned int> keyGenerations;
-  std::map<unsigned int, unsigned int> faceTests;
-  std::map<unsigned int, unsigned int> centerTests;
+  std::map<unsigned int, std::vector<field_type> > timeRatiosKey;
+  std::map<unsigned int, std::vector<field_type> > timeRatiosIteration;
+  std::map<unsigned int, std::vector<field_type> > timeRatiosIntegration;
+  std::map<unsigned int, std::vector<field_type> > elementCountRatios;
+  std::map<unsigned int, unsigned long> keyGenerations;
+  std::map<unsigned int, unsigned long> faceTests;
+  std::map<unsigned int, unsigned long> centerTests;
 
   for (auto numberOfElements : numbersOfElements) {
     // construct Grid
@@ -96,16 +180,23 @@ int main(int argc, char** argv)
       // generate random data
       std::generate(data.begin(), data.end(), [&]() { return distribution(generator); });
       // calculate time for fulltpmc and simpletpmc
-      field_type timeFullTPMC = 0.0;
-      field_type timeSimpleTPMC = 0.0;
+      TimeResult acctimeResultFullTPMC{ 0.0, 0.0, 0.0 , 0.0, 0.0};
+      TimeResult acctimeResultSimpleTPMC{ 0.0, 0.0, 0.0 , 0.0, 0.0};
       for (unsigned int j = 0; j < numberOfRunsPerDataset; ++j) {
-        timeFullTPMC += keyTimeFullTPMC(data);
-        timeSimpleTPMC += keyTimeSimpleTPMC(data);
+        acctimeResultFullTPMC += keyTimeFullTPMC(data);
+        acctimeResultSimpleTPMC += keyTimeSimpleTPMC(data);
       }
-      timeFullTPMC /= numberOfRunsPerDataset;
-      timeSimpleTPMC /= numberOfRunsPerDataset;
+      acctimeResultFullTPMC /= numberOfRunsPerDataset;
+      acctimeResultSimpleTPMC /= numberOfRunsPerDataset;
       // add time ratio to output
-      timeRatios[numberOfElements].push_back(timeFullTPMC / timeSimpleTPMC);
+      timeRatiosKey[numberOfElements].push_back(acctimeResultFullTPMC.key
+                                                / acctimeResultSimpleTPMC.key);
+      timeRatiosIteration[numberOfElements].push_back(acctimeResultFullTPMC.iteration
+                                                      / acctimeResultSimpleTPMC.iteration);
+      timeRatiosIntegration[numberOfElements].push_back(acctimeResultFullTPMC.total
+                                                        / acctimeResultSimpleTPMC.total);
+      elementCountRatios[numberOfElements].push_back(static_cast<double>(acctimeResultFullTPMC.elements)
+                                                     / acctimeResultSimpleTPMC.elements);
     }
     keyGenerations[numberOfElements] = keyTimeFullTPMC.getMC().profKeyGenerations();
     faceTests[numberOfElements] = keyTimeFullTPMC.getMC().profFaceTests();
@@ -117,25 +208,20 @@ int main(int argc, char** argv)
   }
   // output statistics
   std::ofstream output(outputfilename);
-  output << "N min max mean std\n";
-  for (auto x : timeRatios) {
-    field_type min
-        = std::accumulate(x.second.begin(), x.second.end(), std::numeric_limits<field_type>::max(),
-                          [](field_type a, field_type b) { return std::min(a, b); });
-    field_type max
-        = std::accumulate(x.second.begin(), x.second.end(), std::numeric_limits<field_type>::min(),
-                          [](field_type a, field_type b) { return std::max(a, b); });
-    field_type mean = std::accumulate(x.second.begin(), x.second.end(), 0.0) / x.second.size();
-    field_type st = 0.0;
-    for (auto v : x.second) {
-      st += (v - mean) * (v - mean);
-    }
-    st = std::sqrt(st / (x.second.size() - 1));
-    output << x.first << " " << min << " " << max << " " << mean << " " << st << "\n";
-  }
+  output << "time ratios key\n";
+  printStatistics(output, timeRatiosKey);
+  output << "\n";
+  output << "time ratios iteration\n";
+  printStatistics(output, timeRatiosIteration);
+  output << "\n";
+  output << "time ratios total\n";
+  printStatistics(output, timeRatiosIntegration);
+  output << "\n";
+  output << "element count ratios\n";
+  printStatistics(output, elementCountRatios);
   output << "\n";
   output << "N keyGen faceTests relFaceTests centerTests relCenterTests\n";
-  for (auto numberOfElements: numbersOfElements) {
+  for (auto numberOfElements : numbersOfElements) {
     output << numberOfElements << " " << keyGenerations[numberOfElements] << " "
            << faceTests[numberOfElements] << " "
            << static_cast<double>(faceTests[numberOfElements]) / keyGenerations[numberOfElements]
